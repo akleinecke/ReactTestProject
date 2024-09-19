9c9636a89a2938689fb3d9ef5fa04091b605fcde
tooth-juice <kleineckeaustin@gmail.com>
2024-09-18 18:42:15 -0700

    Added testing .css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import { useState } from 'react';
 import './App.css';
 
+interface CustomButtonProperties {
+  title: string;
+  disabled: boolean;
+  click: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function CustomButton({title, disabled, click}: CustomButtonProperties) {
+  return(
+    <button
+    onClick={click}
+    className="App-button"
+    disabled={disabled}>
+      {title}
+    </button>
+  );
+}
+
 function App() {
+  const [count, setCount] = useState(1);
+
+  const handleButtonPress = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App"> 
     {/* We're defining 'className' so that when we import App.css we can grab our definitions.
         This lets us customize what the different 'things' look like using .css! */}
       <header className="App-header">
-        <text>
-          React.js
-        </text>
+        <h1>
+          React.js Header
+        </h1>
       </header>
       <body className="App-body">
-        <text>
+        <p>
           Hey. <br/> {/* <br/> creates a line break! */}
-          This is just a test page. <br/>
+          This is just a body test. <br/>
           Cool, huh? :)
-        </text>
+        </p>
+        <div style={{ height: '50px' }}/> {/* Spacer / Margin */}
+        <p>{count}</p>
+        <div style={{ height: '10px' }}/> {/* Spacer / Margin */}
+        <CustomButton title="Do Not Press" disabled={false} click={handleButtonPress}/>
       </body>
     </div>
   );
